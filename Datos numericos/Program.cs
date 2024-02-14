1bee411c403bb8b50071d26d44e14317b777472c
Bryan <bryanjosueriverahernandez@gmail.com>
2024-02-14 16:29:09 -0600

    Agregar archivos de proyecto.

---
+++ Datos numericos/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese la base del triángulo:");
+double baseTriangulo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la altura del triángulo:");
+double alturaTriangulo = double.Parse(Console.ReadLine());
+
+double areaTriangulo = 0.5 * baseTriangulo * alturaTriangulo;
+
+Console.WriteLine($"El área del triángulo es: {areaTriangulo}");
